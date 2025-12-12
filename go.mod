7aa3dfb5025f6cfc9522fa5c3fc8aec2154942a0
Paulo Machado <pmachado@titanhq.com>
2025-12-12 16:44:55 +0000

    Add IceCreamParlor

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pauloalexandre3d/hackerrank-go
+
+go 1.25.3
---
+++ ice_cream_parlor/ice_cream_parlor_test.go
@@ -0,0 +1,26 @@
+package ice_cream_parlor
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIceCreamParlor(t *testing.T) {
+	menu := []int{2, 7, 13, 5, 4, 13, 5}
+	money := 10
+
+	indices := getIndices(menu, money)
+	fmt.Printf("%+v", indices)
+}
+
+func getIndices(menu []int, money int) []int {
+	seen := make(map[int]int)
+	for i, price := range menu {
+		complement := money - price
+		if j, found := seen[complement]; found {
+			return []int{j, i}
+		}
+		seen[price] = i
+	}
+	return []int{}
+}
