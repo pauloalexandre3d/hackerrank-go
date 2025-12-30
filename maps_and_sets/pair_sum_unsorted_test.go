7c6d6997de5d9c19c17be9b79b538bad932a2512
Paulo Machado <pmachado@titanhq.com>
2025-12-30 17:33:42 +0000

    Add PairSumUnsorted

---
+++ maps_and_sets/pair_sum_unsorted_test.go
@@ -0,0 +1,34 @@
+package maps_and_sets
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPairSumUnsorted(t *testing.T) {
+	tcs := []struct {
+		in     []int
+		target int
+		out    []int
+	}{
+		{[]int{-1, 3, 4, 2}, 3, []int{0, 2}},
+	}
+	for _, tc := range tcs {
+		got := pairSumUnsorted(tc.in, tc.target)
+		if !assert.Equal(t, tc.out, got) {
+			t.Errorf("input: %v, got %v, want %v", tc.in, got, tc.out)
+		}
+	}
+}
+
+func pairSumUnsorted(in []int, target int) []int {
+	hm := make(map[int]int)
+	for i, x := range in {
+		if _, exists := hm[target-x]; exists {
+			return []int{hm[target-x], i}
+		}
+		hm[x] = i
+	}
+	return []int{}
+}
